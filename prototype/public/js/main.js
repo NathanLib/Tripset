e8f04c2dbc332273e0005e1b375fc51122f4b84c
Nathan Liboutet <nathanliboutet@Nathans-MBP.lan>
2020-02-12 20:37:10 +0000

    menu coding: 50%

---
+++ prototype/public/js/main.js
@@ -0,0 +1,15 @@
+/* =======================================================================
+
+                                 General
+
+   ======================================================================= */
+
+/* ============================= Header ================================== */
+function openNav() {
+    document.getElementById("mobileNav").style.width = "50vw";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+function closeNav() {
+    document.getElementById("mobileNav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
