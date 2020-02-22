b183d6e9d8f15cbdc9648c771db2aeb603897e3d
Nathan <nathan.lib19@gmail.com>
2020-02-22 22:25:06 +0000

    End transfer dates between inputs and calendar

--- prototype/public/js/main.js
+++ prototype/public/js/main.js
@@ -4,6 +4,8 @@
 
    ======================================================================= */
 
+const isNull = (value) => typeof value === "object" && !value;
+
 /* ============================= Header ================================== */
 function openNav() {
     $('.mobileNav').css({
@@ -35,86 +37,145 @@ function closeNav() {
 
 /* ================== Searchbar - Dates details ======================= */
 
-function addDates() {
-    var startDate = new Date();
-    var numberOfColumns = 1;
-    var width = document.body.clientWidth;
+var dayFrom = $('#dayFrom');
+var monthFrom = $('#monthFrom');
+var yearFrom = $('#yearFrom');
+var dayTo = $('#dayTo');
+var monthTo = $('#monthTo');
+var yearTo = $('#yearTo');
 
-    var dayFrom = $('#dayFrom');
-    var monthFrom = $('#monthFrom');
-    var yearFrom = $('#yearFrom');
-    var dayTo = $('#dayTo');
-    var monthTo = $('#monthTo');
-    var yearTo = $('#yearTo');
+var myPicker = null;
+
+function numberOfColumns() {
+    var width = document.body.clientWidth;
+    var numberOfColumns = 1;
 
     if (width >= 992) {
         numberOfColumns = 2;
     }
 
-    if ($('#litepicker').is(':empty')) {
-        var picker = new Litepicker({
-            element: document.getElementById('litepicker'),
-            singleMode: false,
-            allowRepick: true,
-            inlineMode: true,
-            numberOfMonths: 2,
-            numberOfColumns: numberOfColumns,
-            startDate: startDate,
-            minDate: startDate,
-            autoApply: true,
-            showWeekNumbers: true,
-            mobileFriendly: true,
-            onSelect: (date1, date2) => {
-                dayFrom.html(date1.format('DD'));
-                dayFrom.val(date1.format('DD'));
-                monthFrom.html(date1.format('MM'));
-                monthFrom.val(date1.format('MM'));
-                yearFrom.html(date1.format('YYYY'));
-                yearFrom.val(date1.format('YYYY'));
-
-                dayTo.html(date2.format('DD'));
-                dayTo.val(date2.format('DD'));
-                monthTo.html(date2.format('MM'));
-                monthTo.val(date2.format('MM'));
-                yearTo.html(date2.format('YYYY'));
-                yearTo.val(date2.format('YYYY'));
-            }
-        });
-    }
-
-    $('.dates-details').slideToggle(700);
+    return numberOfColumns;
 }
 
-function datesValidation() {}
 
-$('#submit').click(function() {
-    var date = parseInt($('#date').val());
-    var month = parseInt($('#month').val());
-    var year = parseInt($('#year').val());
-    if (isNaN(date) || isNaN(month) || isNaN(year)) {
-        alert('wrong format');
-        return false;
-    } else {
-        if (date > 31 || date < 1) {
-            alert('wrong date');
-            return false;
-        } else if ((month == 4 || month == 6 || month == 9 || month == 11) && date == 31) {
-            alert('wrong date');
-            return false;
-        } else if (month == 2) {
-            var isleap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
-            if (date > 29 || (date == 29 && !isleap))
-                alert('wrong date');
-            return false;
+function createCalendar() {
+    var picker = new Litepicker({
+        element: document.getElementById('litepicker'),
+        singleMode: false,
+        allowRepick: true,
+        inlineMode: true,
+        numberOfMonths: 2,
+        numberOfColumns: numberOfColumns(),
+        minDate: new Date(),
+        autoApply: true,
+        showWeekNumbers: true,
+        mobileFriendly: true,
+        onSelect: (date1, date2) => {
+            dayFrom.html(date1.format('DD'));
+            dayFrom.val(date1.format('DD'));
+            monthFrom.html(date1.format('MM'));
+            monthFrom.val(date1.format('MM'));
+            yearFrom.html(date1.format('YYYY'));
+            yearFrom.val(date1.format('YYYY'));
+
+            dayTo.html(date2.format('DD'));
+            dayTo.val(date2.format('DD'));
+            monthTo.html(date2.format('MM'));
+            monthTo.val(date2.format('MM'));
+            yearTo.html(date2.format('YYYY'));
+            yearTo.val(date2.format('YYYY'));
         }
-        if (month > 12 || month < 1) {
-            alert('wrong month');
-            return false;
+    });
+}
+
+function createCalendarWithRange(startDate, endDate) {
+    $("#litepicker").children().remove();
+
+    var picker = new Litepicker({
+        element: document.getElementById('litepicker'),
+        singleMode: false,
+        allowRepick: true,
+        inlineMode: true,
+        numberOfMonths: 2,
+        numberOfColumns: numberOfColumns(),
+        minDate: new Date(),
+        autoApply: true,
+        showWeekNumbers: true,
+        mobileFriendly: true,
+
+        startDate: startDate,
+        endDate: endDate,
+
+        onSelect: (date1, date2) => {
+            dayFrom.html(date1.format('DD'));
+            dayFrom.val(date1.format('DD'));
+            monthFrom.html(date1.format('MM'));
+            monthFrom.val(date1.format('MM'));
+            yearFrom.html(date1.format('YYYY'));
+            yearFrom.val(date1.format('YYYY'));
+
+            dayTo.html(date2.format('DD'));
+            dayTo.val(date2.format('DD'));
+            monthTo.html(date2.format('MM'));
+            monthTo.val(date2.format('MM'));
+            yearTo.html(date2.format('YYYY'));
+            yearTo.val(date2.format('YYYY'));
         }
-        if (year > 2050 || year < 1900) {
-            alert('wrong year');
-            return false;
-        }
+    });
+}
+
+function manageCalendar() {
+    if ($('#litepicker').is(':empty')) {
+        createCalendar();
+    } else if (dayFrom.val() != "" && monthFrom.val() != "" && yearFrom.val() != "" &&
+        dayTo.val() != "" && monthTo.val() != "" && yearTo.val() != "") {
+
+        // -1 because month Integer value representing the month, beginning with 0 for January to 11 for December.
+        var startDate = new Date(yearFrom.val(), monthFrom.val() - 1 , dayFrom.val());
+        var endDate = new Date(yearTo.val(), monthTo.val() - 1, dayTo.val());
+
+        createCalendarWithRange(startDate, endDate);
     }
-    $('#myform').submit();
-});
+}
+
+function addDates() {
+    $.when(
+        manageCalendar()
+    ).done(function() {
+        $('.dates-details').slideToggle(700);
+    });
+}
+
+function datesValidation() {
+    $('#submit').click(function() {
+        var date = parseInt($('#date').val());
+        var month = parseInt($('#month').val());
+        var year = parseInt($('#year').val());
+        if (isNaN(date) || isNaN(month) || isNaN(year)) {
+            alert('wrong format');
+            return false;
+        } else {
+            if (date > 31 || date < 1) {
+                alert('wrong date');
+                return false;
+            } else if ((month == 4 || month == 6 || month == 9 || month == 11) && date == 31) {
+                alert('wrong date');
+                return false;
+            } else if (month == 2) {
+                var isleap = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+                if (date > 29 || (date == 29 && !isleap))
+                    alert('wrong date');
+                return false;
+            }
+            if (month > 12 || month < 1) {
+                alert('wrong month');
+                return false;
+            }
+            if (year > 2050 || year < 1900) {
+                alert('wrong year');
+                return false;
+            }
+        }
+        $('#myform').submit();
+    });
+}
