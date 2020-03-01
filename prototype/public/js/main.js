ad84b68dd2a398c45189a856e7f8e405e882ef1e
Nathan <nathan.lib19@gmail.com>
2020-03-01 18:46:32 +0000

    Change location of Jamie files to the right folder

--- prototype/public/js/main.js
+++ prototype/public/js/main.js
@@ -483,3 +483,44 @@ function eventTitle(element, length) {
             .text(element.find(".event-content-title").text() + "...");
     }
 }
+
+/* =======================================================================
+
+                                Login page
+
+   ======================================================================= */
+("use strict");
+$(".js-1").click(function() {
+    $(".signup-holder")
+        .fadeIn(600)
+        .css({
+            display: "flex"
+        });
+    $(".login-holder").fadeOut(0);
+});
+
+("use strict");
+$(".js-2").click(function() {
+    $(".login-holder").fadeIn(600);
+    $(".signup-holder").fadeOut(0);
+});
+
+("use strict");
+
+function check(form) {
+    if (form.uname.value == "user" && form.psw.value == "123") {
+        alert("Welcome User");
+    } else {
+        alert("The username or password didn't match");
+    }
+}
+
+function checkBox() {
+    if ($("#option").is(":checked")) {
+        alert("Thanks for joining us!");
+        $(".signup-holder").fadeOut(0);
+        $(".login-holder").fadeIn(600);
+    } else {
+        alert("You didn't agree to our terms :(");
+    }
+}
