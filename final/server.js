b23bcc9d5e657e4f066c63abda71d6e36d2855dc
Nathan <nathan.lib19@gmail.com>
2020-03-28 20:14:03 +0100

    login (still need controls)

--- final/server.js
+++ final/server.js
@@ -5,8 +5,7 @@ const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
-const salt = bcrypt.genSaltSync(saltRounds);
+const salt = "$2b$10$YJ6Y1PnM4KQzPyAr3gjj3e";
 
 const app = express();
 const port = 8080;
@@ -77,73 +76,77 @@ app.get("/resetPwd", function(req, res) {
     res.render("pages/resetPwd");
 });
 
-// /*************************** POST ROUTES **************************
+//logout route cause the page to Logout.
+//it sets our session.loggedin to false and then redirects the user to the login
+app.get("/logout", function(req, res) {
+    req.session.loggedin = false;
+    console.log(req.session);
+    req.session.destroy();
+    console.log(req.session);
+    res.redirect("/");
+});
 
-//the dologin route detasl with the data from the login screen.
-//the post variables, username and password ceom from the form on the login page.
-// app.post("/dologin", function(req, res) {
-//     console.log(JSON.stringify(req.body));
+//*************************** POST ROUTES **************************
 
-//     var email = req.body.email;
-//     var pword = req.body.password;
+// the dologin route detasl with the data from the login screen.
+// the post variables, username and password ceom from the form on the login page.
+app.post("/dologin", function(req, res) {
+    var email = req.body.email;
+    var pword = req.body.password;
 
-//     db.collection("profiles").findOne({ "login.email": email }, function(
-//         err,
-//         result
-//     ) {
-//         if (err) throw err; //if there is an error, throw the error
-//         //if there is no result, redirect the user back to the login system as that email must not exist
-//         if (!result) {
-//             res.redirect("/login");
-//             return;
-//         }
+    db.collection("profiles").findOne({ "login.email": email }, function(
+        err,
+        result
+    ) {
+        if (err) throw err; //if there is an error, throw the error
 
-//         //encrypt the password from the form before compare it with database
-//         const hash = bcrypt.hashSync(pword, salt);
-//         console.log(hash + "\n" + result.login.password);
+        //if there is no result, redirect the user back to the login system as that email must not exist
+        if (!result) {
+            res.redirect("/login");
+            return;
+        }
 
-//         //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
-//         bcrypt.compare(hash, result.login.password, function(err, result) {
-//             if (result === true) {
-//                 req.session.loggedin = true;
-//                 req.session.email = email;
-//                 console.log(req.session);
-//                 res.redirect("/profile");
-//             }
-//             //otherwise send them back to login
-//             else {
-//                 res.redirect("/login");
-//             }
-//         });
-//     });
+        //encrypt the password from the form before compare it with database
+        const hash = bcrypt.hashSync(pword, salt);
 
-//     res.redirect("/profile");
-// });
+        //if there is a result then check the password, if the password is correct set session loggedin to true and send the user to the index
+        if (hash == result.login.password) {
+            req.session.loggedin = true;
+            req.session.email = result.login.email;
 
-// app.post("/dosignup", function(req, res) {
-//     var pword = req.body.password;
+            res.redirect("/profile");
+        }
+        //otherwise send them back to login
+        else {
+            res.redirect("/login");
+        }
+    });
+});
 
-//     //encrypt the password from the form before inserting it with database
-//     const hash = bcrypt.hashSync(pword, salt);
+app.post("/dosignup", function(req, res) {
+    var pword = req.body.password;
 
-//     var datatostore = {
-//         name: {
-//             first: req.body.firstname,
-//             last: req.body.lastname
-//         },
-//         login: {
-//             email: req.body.email,
-//             password: hash
-//         },
-//         favourites: [],
-//         historic: []
-//     };
+    //encrypt the password from the form before inserting it with database
+    const hash = bcrypt.hashSync(pword, salt);
 
-//     //once created we just run the data string against the database and all our new data will be saved
-//     db.collection("profiles").insertOne(datatostore, function(err, result) {
-//         if (err) throw err;
-//         console.log("Saved to database");
-//         //when complete redirect to the login page
-//         res.redirect("/login");
-//     });
-// });
+    var datatostore = {
+        name: {
+            first: req.body.firstname,
+            last: req.body.lastname
+        },
+        login: {
+            email: req.body.email,
+            password: hash
+        },
+        favourites: [],
+        historic: []
+    };
+
+    //once created we just run the data string against the database and all our new data will be saved
+    db.collection("profiles").insertOne(datatostore, function(err, result) {
+        if (err) throw err;
+        console.log("Saved to database");
+        //when complete redirect to the login page
+        res.redirect("/login");
+    });
+});
